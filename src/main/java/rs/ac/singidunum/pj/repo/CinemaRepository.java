e839c7555f58f39990cc7b4f04bbe7df72266e62
milanr1225 <40809226+milanr1225@users.noreply.github.com>
2026-05-29 22:14:45 +0200

    Initial commit

---
+++ src/main/java/rs/ac/singidunum/pj/repo/CinemaRepository.java
@@ -0,0 +1,9 @@
+package rs.ac.singidunum.pj.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.ac.singidunum.pj.entity.Cinema;
+
+@Repository
+public interface CinemaRepository extends JpaRepository<Cinema, Integer> {
+}
