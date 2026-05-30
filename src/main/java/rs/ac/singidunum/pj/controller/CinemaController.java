fcdd551f35640023bc4d9a0f899da5fa4d360196
milanr1225 <40809226+milanr1225@users.noreply.github.com>
2026-05-30 13:47:35 +0200

    Added Create and Update with some minor style changes

--- src/main/java/rs/ac/singidunum/pj/controller/CinemaController.java
+++ src/main/java/rs/ac/singidunum/pj/controller/CinemaController.java
@@ -2,6 +2,9 @@ package rs.ac.singidunum.pj.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +16,12 @@ import rs.ac.singidunum.pj.repo.CinemaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -27,7 +36,32 @@ public class CinemaController {
         return repository.findAllByDeletedAtIsNull();
     }
 
+    @GetMapping(path ="/{id}")
+    public ResponseEntity<Cinema> getCinemaById(@PathVariable Integer id) {
+        return ResponseEntity.of(repository.findOneByCinemaIdAndDeletedAtIsNull(id));
+    }
+
+    @PostMapping
+    public Cinema createCinema(@RequestBody Cinema entity) {
+        Cinema cinema = new Cinema();
+        cinema.setName(entity.getName());
+        cinema.setAddress(entity.getAddress());
+        cinema.setCreatedAt(LocalDateTime.now());
+        return repository.save(cinema);
+    }
+
+    @PutMapping(path ="/{id}")
+    public Cinema updateCinema(@PathVariable Integer id,@RequestBody Cinema entity) {
+        Cinema cinema = repository.findOneByCinemaIdAndDeletedAtIsNull(id).orElseThrow();
+        cinema.setName(entity.getName());
+        cinema.setAddress(entity.getAddress());
+        cinema.setUpdatedAt(LocalDateTime.now());
+        return repository.save(cinema);
+    }
+    
+
     @DeleteMapping(path = "/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteCinemaById(@PathVariable Integer id) {
         Cinema cinema = repository.findOneByCinemaIdAndDeletedAtIsNull(id).orElseThrow();
         cinema.setDeletedAt(LocalDateTime.now());
